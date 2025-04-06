05456b4db07fa96682e71b82881895c37d2e5125
Yoshiip <27858195+Yoshiip@users.noreply.github.com>
2025-04-06 23:27:25 +0200

    update

--- main/go/camera.go
+++ main/go/camera.go
@@ -1,11 +1,12 @@
 embedded_components {
   id: "camera"
   type: "camera"
-  data: "aspect_ratio: 1.7777778\n"
-  "fov: 10.0\n"
-  "near_z: 0.1\n"
+  data: "aspect_ratio: 1.0\n"
+  "fov: 0.7854\n"
+  "near_z: 0.0\n"
   "far_z: 1000.0\n"
   "auto_aspect_ratio: 1\n"
   "orthographic_projection: 1\n"
+  "orthographic_zoom: 4.0\n"
   ""
 }
