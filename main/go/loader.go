f1e2f886b215d672656b8c492046e09a7601a805
Yoshiip <27858195+Yoshiip@users.noreply.github.com>
2025-04-08 00:02:04 +0200

    doors system

---
+++ main/go/loader.go
@@ -0,0 +1,34 @@
+components {
+  id: "config"
+  component: "/main/scripts/config.script"
+}
+embedded_components {
+  id: "batfactory"
+  type: "factory"
+  data: "prototype: \"/main/monsters/bat.go\"\n"
+  ""
+}
+embedded_components {
+  id: "camerafactory"
+  type: "factory"
+  data: "prototype: \"/main/go/camera.go\"\n"
+  ""
+}
+embedded_components {
+  id: "doors_factory"
+  type: "factory"
+  data: "prototype: \"/main/go/door.go\"\n"
+  ""
+}
+embedded_components {
+  id: "layerfactory"
+  type: "factory"
+  data: "prototype: \"/main/go/layer.go\"\n"
+  ""
+}
+embedded_components {
+  id: "lightsfactory"
+  type: "factory"
+  data: "prototype: \"/main/go/light.go\"\n"
+  ""
+}
